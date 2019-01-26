a19fe1cea8eab8c05eff898a01c846fa10ee8e3c
Blue <bluestorm.android@gmail.com>
2019-01-26 19:53:09 +0100

    Exception Constants

---
+++ ProjectPSX/PSX/EX.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPSX {
+    static class EX {
+        public const uint LOAD_ADRESS_ERROR = 0x4;
+        public const uint STORE_ADRESS_ERROR = 0x5;
+        public const uint SYSCALL = 0x8;
+        public const uint OVERFLOW = 0xC;
+    }
+}
