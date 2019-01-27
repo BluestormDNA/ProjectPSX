6004122c507652709910aa06305c53cad20fcfdb
Blue <bluestorm.android@gmail.com>
2019-01-27 23:02:43 +0100

    Added More Exceptions

--- ProjectPSX/PSX/EX.cs
+++ ProjectPSX/PSX/EX.cs
@@ -9,6 +9,8 @@ namespace ProjectPSX {
         public const uint LOAD_ADRESS_ERROR = 0x4;
         public const uint STORE_ADRESS_ERROR = 0x5;
         public const uint SYSCALL = 0x8;
+        public const uint BREAK = 0x9;
+        public const uint COPROCESSOR_ERROR = 0xB;
         public const uint OVERFLOW = 0xC;
     }
 }
