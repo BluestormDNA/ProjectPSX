a61140655c6cdf3c4708f6520e8817b52697be26
Blue <bluestorm.android@gmail.com>
2019-04-21 15:37:58 +0200

    Add interrupt enum

--- ProjectPSX/Util/Constants.cs
+++ ProjectPSX/Util/Constants.cs
@@ -1,6 +1,7 @@
 ﻿namespace ProjectPSX {
 
     public enum EX {
+        INTERRUPT = 0x0,
         LOAD_ADRESS_ERROR = 0x4,
         STORE_ADRESS_ERROR = 0x5,
         SYSCALL = 0x8,
@@ -8,6 +9,20 @@
         ILLEGAL_INSTR = 0xA,
         COPROCESSOR_ERROR = 0xB,
         OVERFLOW = 0xC
+    }
+
+    public enum Interrupt {
+        VBLANK = 0x1,
+        GPU = 0x2,
+        CDROM = 0x4,
+        DMA = 0x8,
+        TIMER0 = 0x10,
+        TIMER1 = 0x20,
+        TIMER2 = 0x40,
+        CONTR = 0x80,
+        SIO = 0x100,
+        SPU = 0x200,
+        PIO = 0x400,
     }
 
     public enum Width {
