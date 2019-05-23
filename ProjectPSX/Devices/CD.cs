f85d99703a5bccf623bb388283d78cb71afbb01a
Blue <bluestorm.android@gmail.com>
2019-05-23 10:31:08 +0200

    RAW offset reads fix

--- ProjectPSX/Devices/CD.cs
+++ ProjectPSX/Devices/CD.cs
@@ -7,6 +7,7 @@ namespace ProjectPSX.Devices {
         private const int BYTES_PER_SECTOR_RAW = 2352;
         private const int BYTES_PER_SECTOR_DATA = 2048;
         private const int BYTES_PER_SECTOR_HEADER = 24;
+        private const int BYTES_PER_SECTOR_RAW_SYNC_HEADER = 12;
 
         private FileStream stream;
         private BinaryReader reader;
@@ -21,17 +22,28 @@ namespace ProjectPSX.Devices {
             int bytesToRead;
             int sectorHeaderOffset;
             if (isSectorSizeRAW){
-                bytesToRead = BYTES_PER_SECTOR_RAW;
-                sectorHeaderOffset = 0;
+                bytesToRead = BYTES_PER_SECTOR_RAW - BYTES_PER_SECTOR_RAW_SYNC_HEADER;
+                sectorHeaderOffset = BYTES_PER_SECTOR_RAW_SYNC_HEADER;
+                //Console.WriteLine("[CD] WARNING RAW READ !!!");
+                //Console.ReadLine();
             } else {
                 bytesToRead = BYTES_PER_SECTOR_DATA;
                 sectorHeaderOffset = BYTES_PER_SECTOR_HEADER;
             }
 
-            stream.Seek(loc * BYTES_PER_SECTOR_RAW + sectorHeaderOffset, 0);
+            long read = stream.Seek(loc * BYTES_PER_SECTOR_RAW + sectorHeaderOffset, 0);
+
             //Console.WriteLine("LOC = " + loc  + " " + ((loc * BYTES_PER_SECTOR_RAW) + sectorHeaderOffset).ToString("x8"));
-            //Console.ReadLine();
-            return reader.ReadBytes(bytesToRead);
+
+            byte[] ret = reader.ReadBytes(bytesToRead);
+
+            //TODO: Beyond size reads return 0 reads //retrite this?
+            if (ret.Length == 0) {
+                Console.WriteLine("[CD] READ BEYOND SIZE! returning 0");
+                return new byte[bytesToRead];
+            }
+
+            return ret;
         }
 
     }
