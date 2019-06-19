be8f58a09b424b1420ddca60b81c3fb6c983eed1
Blue <bluestorm.android@gmail.com>
2019-06-19 18:12:09 +0200

    Minor Changes: inverted 0 if.

--- ProjectPSX/Devices/CD.cs
+++ ProjectPSX/Devices/CD.cs
@@ -4,46 +4,42 @@ using System;
 namespace ProjectPSX.Devices {
     internal class CD {
 
-        private const int BYTES_PER_SECTOR_RAW = 2352;
-        private const int BYTES_PER_SECTOR_DATA = 2048;
-        private const int BYTES_PER_SECTOR_HEADER = 24;
-        private const int BYTES_PER_SECTOR_RAW_SYNC_HEADER = 12;
+        private const int BytesPerSectorRaw = 2352;
+        private const int BytesPerSectorData = 2048;
+        private const int BytesPerSectorHeader = 24;
+        private const int BytesPerSectorRawSyncHeader = 12;
 
         private FileStream stream;
         private BinaryReader reader;
 
         public CD() {
-            //temporary hard coded
-            stream = new FileStream("./crash.bin", FileMode.Open, FileAccess.Read);
+            //temporary hard coded Ideally this should be set from a simple drag and drop to the emu
+            stream = new FileStream("./rr.bin", FileMode.Open, FileAccess.Read);
             reader = new BinaryReader(stream);
         }
 
-        public byte[] read(bool isSectorSizeRAW, int loc) {
+        public byte[] Read(bool isSectorSizeRaw, int loc) {
             int bytesToRead;
             int sectorHeaderOffset;
-            if (isSectorSizeRAW){
-                bytesToRead = BYTES_PER_SECTOR_RAW - BYTES_PER_SECTOR_RAW_SYNC_HEADER;
-                sectorHeaderOffset = BYTES_PER_SECTOR_RAW_SYNC_HEADER;
+            if (isSectorSizeRaw){
+                bytesToRead = BytesPerSectorRaw - BytesPerSectorRawSyncHeader;
+                sectorHeaderOffset = BytesPerSectorRawSyncHeader;
                 //Console.WriteLine("[CD] WARNING RAW READ !!!");
                 //Console.ReadLine();
             } else {
-                bytesToRead = BYTES_PER_SECTOR_DATA;
-                sectorHeaderOffset = BYTES_PER_SECTOR_HEADER;
+                bytesToRead = BytesPerSectorData;
+                sectorHeaderOffset = BytesPerSectorHeader;
             }
 
-            long read = stream.Seek(loc * BYTES_PER_SECTOR_RAW + sectorHeaderOffset, 0);
+            long read = stream.Seek(loc * BytesPerSectorRaw + sectorHeaderOffset, 0);
 
             //Console.WriteLine("LOC = " + loc  + " " + ((loc * BYTES_PER_SECTOR_RAW) + sectorHeaderOffset).ToString("x8"));
 
             byte[] ret = reader.ReadBytes(bytesToRead);
+            if (ret.Length != 0) return ret;
 
-            //TODO: Beyond size reads return 0 reads //retrite this?
-            if (ret.Length == 0) {
-                Console.WriteLine("[CD] READ BEYOND SIZE! returning 0");
-                return new byte[bytesToRead];
-            }
-
-            return ret;
+            Console.WriteLine("[CD] READ BEYOND SIZE! returning zeros");
+            return new byte[bytesToRead];
         }
 
     }
