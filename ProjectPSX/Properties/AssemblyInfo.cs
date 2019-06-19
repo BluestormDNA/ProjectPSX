4d358aa2f59724df052526e2d39f2e0b0f778d4a
Blue <bluestorm.android@gmail.com>
2019-06-19 18:16:45 +0200

    Update

--- ProjectPSX/Properties/AssemblyInfo.cs
+++ ProjectPSX/Properties/AssemblyInfo.cs
@@ -1,5 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // La información general de un ensamblado se controla mediante el siguiente 
