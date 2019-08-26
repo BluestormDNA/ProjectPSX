957f7431b1cc0e3e711321ffc1955fe25f809827
Blue <bluestorm.android@gmail.com>
2019-08-26 21:27:12 +0200

    Add: Extension method to cdrom to enqueue lists.

---
+++ ProjectPSX/Util/Extension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectPSX.Devices {
+    static class Extension {
+        public static void EnqueueRange<T>(this Queue<T> queue, params T[] parameters) {
+            foreach (T parameter in parameters)
+                queue.Enqueue(parameter);
+        }
+    }
+}
