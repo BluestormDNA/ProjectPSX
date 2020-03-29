e79a0e673bf736c4e9532a771cb56b5b6229478b
Blue <bluestorm.android@gmail.com>
2020-03-29 21:33:13 +0200

    InterruptController: Interpolate strings

--- ProjectPSX/Devices/InterruptController.cs
+++ ProjectPSX/Devices/InterruptController.cs
@@ -8,32 +8,32 @@ namespace ProjectPSX.Devices {
 
         internal void set(Interrupt interrupt) {
             ISTAT |= (uint)interrupt;
-            //Console.WriteLine("ISTAT SET MANUAL FROM DEVICE: " + ISTAT.ToString("x8") + " IMASK " + IMASK.ToString("x8"));
+            //Console.WriteLine($"ISTAT SET MANUAL FROM DEVICE: {ISTAT:x8} IMASK {IMASK:x8}");
         }
 
 
         internal void writeISTAT(uint value) {
             ISTAT &= value & 0x7FF;
             //Console.ForegroundColor = ConsoleColor.Magenta;
-            //Console.WriteLine("[IRQ] [ISTAT] Write " + value.ToString("x8") + " ISTAT " + ISTAT.ToString("x8"));
+            //Console.WriteLine($"[IRQ] [ISTAT] Write {value:x8} ISTAT {ISTAT:x8}");
             //Console.ResetColor();
             //Console.ReadLine();
         }
 
         internal void writeIMASK(uint value) {
             IMASK = value & 0x7FF;
-            //Console.WriteLine("[IRQ] [IMASK] Write " + IMASK.ToString("x8"));
+            //Console.WriteLine($"[IRQ] [IMASK] Write {IMASK:x8}");
             //Console.ReadLine();
         }
 
         internal uint loadISTAT() {
-            //Console.WriteLine("[IRQ] [ISTAT] Load " + ISTAT.ToString("x8"));
+            //Console.WriteLine($"[IRQ] [ISTAT] Load {ISTAT:x8}");
             //Console.ReadLine();
             return ISTAT;
         }
 
         internal uint loadIMASK() {
-            //Console.WriteLine("[IRQ] [IMASK] Load " + IMASK.ToString("x8"));
+            //Console.WriteLine($"[IRQ] [IMASK] Load {IMASK:x8}");
             //Console.ReadLine();
             return IMASK;
         }
