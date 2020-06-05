efc2c04b2de53169cb6c27ed787b58bbd178e078
Blue <bluestorm.android@gmail.com>
2020-06-05 20:30:43 +0200

    CD: Use ttacks from bin or cue parser when available

--- ProjectPSX/Devices/CD.cs
+++ ProjectPSX/Devices/CD.cs
@@ -2,6 +2,9 @@
 using System;
 using System.Linq;
 using System.Windows.Forms;
+using ProjectPSX.Util;
+using System.Collections.Generic;
+using static ProjectPSX.Util.TrackBuilder;
 
 namespace ProjectPSX.Devices {
     internal class CD {
@@ -15,31 +18,34 @@ namespace ProjectPSX.Devices {
         private byte[] rawSectorBuffer = new byte[BytesPerSectorRaw - BytesPerSectorRawSyncHeader];
         private byte[] dataSectorBuffer = new byte[BytesPerSectorData + BytesPerSubChannelInfo];
 
-        private FileStream stream;
-        private string CdFilePath;
-        private int lba;
+        public List<Track> tracks;
 
         public CD() {
 
             var cla = Environment.GetCommandLineArgs();
             if (cla.Any(s => s.EndsWith(".bin"))) {
-                CdFilePath = cla.First(s => s.EndsWith(".bin"));
+                String file = cla.First(s => s.EndsWith(".bin"));
+                tracks = TrackBuilder.fromBin(file);
             } else {
                 //Show the user a dialog so they can pick the bin they want to load.
-                var file = new OpenFileDialog();
-                file.Filter = "BIN files (*.bin)|*.bin";
-                file.ShowDialog();
-                CdFilePath = file.FileName;
+                var fileDialog = new OpenFileDialog();
+                fileDialog.Filter = "BIN/CUE files (*.bin, *.cue)|*.bin;*.cue";
+                fileDialog.ShowDialog();
+
+                string file = fileDialog.FileName;
+                string ext = Path.GetExtension(file);
+
+                if(ext == ".bin") {
+                    tracks = TrackBuilder.fromBin(file);
+                } else if (ext == ".cue"){
+                    tracks = TrackBuilder.fromCue(file);
+                }
             }
-
-            stream = new FileStream(CdFilePath, FileMode.Open, FileAccess.Read);
-
-            lba = (int)(stream.Length / BytesPerSectorRaw);
-
-            Console.WriteLine($"[CD] LBA: {lba:x8}");
         }
 
         public byte[] Read(bool isSectorSizeRaw, int loc) {
+            string currentTrack = getTrackFromLoc(loc);
+            using FileStream stream = new FileStream(currentTrack, FileMode.Open, FileAccess.Read);
             stream.Seek(loc * BytesPerSectorRaw + BytesPerSectorRawSyncHeader, SeekOrigin.Begin);
             if (isSectorSizeRaw) {
                 stream.Read(rawSectorBuffer, 0, rawSectorBuffer.Length);
@@ -48,9 +54,25 @@ namespace ProjectPSX.Devices {
                 stream.Read(dataSectorBuffer, 0, dataSectorBuffer.Length);
                 return dataSectorBuffer;
             }
+
+        }
+
+        private string getTrackFromLoc(int loc) {
+            foreach(Track track in tracks) {
+                //Console.WriteLine(loc + " " + track.file + track.lbaEnd);
+                if (track.lbaEnd > loc) return track.file;
+            }
+            Console.WriteLine("[CD] WARNING: LBA beyond tracks!");
+            return tracks[0].file; //and explode ¯\_(ツ)_/¯ 
         }
 
         public int getLBA() {
+            int lba = 150;
+
+            foreach (Track track in tracks) {
+                lba += track.lba;
+            }
+            Console.WriteLine($"[CD] LBA: {lba:x8}");
             return lba;
         }
 
