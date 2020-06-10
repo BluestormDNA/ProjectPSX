5a8803143b20c9ddd71ea5d82e0b6ecac2d98d2c
Blue <bluestorm.android@gmail.com>
2020-06-10 22:15:02 +0200

    CDROM: Add XaAdpcm Decoder stub

---
+++ ProjectPSX/Devices/CDROM/XaAdpcm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectPSX.Devices.CdRom {
+    public class XaAdpcm {
+        public static byte[] Decode(byte[] xaadpcm) {
+            //todo decode xadpcm into 16 bit 44100 audio
+            return new byte[] { };
+        }
+    }
+}
