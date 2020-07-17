b8bbd15709f545ca51706c54ade7bb3df7020e77
Blue <bluestorm.android@gmail.com>
2020-07-17 22:52:29 +0200

    Voice: Setup registers, Decode Spu Adpcm

--- ProjectPSX/Devices/SPU/Voice.cs
+++ ProjectPSX/Devices/SPU/Voice.cs
@@ -1,25 +1,76 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Runtime.Loader;
 
 namespace ProjectPSX.Devices.Spu {
     public class Voice {
-        public ushort volumeLeft;           //0
-        public ushort volumeRight;          //2
+
+        private static int[] positiveXaAdpcmTable = new int[] { 0, 60, 115, 98, 122 };
+        private static int[] negativeXaAdpcmTable = new int[] { 0, 0, -52, -55, -60 };
+
+        public struct Volume {
+            public ushort register;
+            public bool isSweepMode => ((register >> 15) & 0x1) != 0;
+            public short fixedVolume => (short)(register << 1);
+            public bool isSweepExponential => ((register >> 14) & 0x1) != 0;
+            public bool isSweepDirectionDecrease => ((register >> 13) & 0x1) != 0;
+            public bool isSweepPhaseNegative => ((register >> 12) & 0x1) != 0;
+            public int sweepShift => (register >> 2) & 0x1F;
+            public int sweepStep => register & 0x3;
+        }
+        public Volume volumeLeft;           //0
+        public Volume volumeRight;          //2
+
         public ushort pitch;                //4
         public ushort startAddress;         //6
         public ushort currentAddress;       //6 Internal
-        public ushort adsrLo;               //8
-        public ushort adsrHi;               //A
-        public ushort adsrVolume;           //C
+
+        public struct ADSR {
+            public ushort lo;               //8
+            public ushort hi;               //A
+            public bool isAttackModeExponential => ((lo >> 15) & 0x1) != 0;
+            public int attackShift => (lo >> 10) & 0x1F;
+            public int attackStep => (lo >> 8) & 0x3; //"+7,+6,+5,+4"
+            public int decayShift => (lo >> 4) & 0xF;
+            public int sustainLevel => lo & 0xF; //Level=(N+1)*800h
+
+            public bool isSustainModeExponential => ((hi >> 15) & 0x1) != 0;
+            public bool isSustainDirectionDecrease => ((hi >> 14) & 0x1) != 0;
+            public int sustainShift => (hi >> 8) & 0x1F;
+            public int sustainStep => (hi >> 6) & 0x3;
+            public bool isReleaseModeExponential => ((hi >> 5) & 0x1) != 0;
+            public int releaseShift => (hi) & 0x1F;
+        }
+        public ADSR adsr;
+
+        public short adsrVolume;            //C
         public ushort adpcmRepeatAddress;   //E
+
+        public struct Counter {            //internal
+            public uint register;
+            public uint currentSampleIndex {
+                get { return (register >> 12) & 0x1F; }
+                set {
+                    register = (ushort)(register &= 0xFFF);
+                    register |= value << 12;
+                }
+            }
+
+            public uint interpolationIndex => (register >> 3) & 0xFF;
+        }
+        public Counter counter;
 
         public Phase adsrPhase;
 
         public short old;
         public short older;
-        public short oldest;
 
+        public short lastBlockSample26;
+        public short lastBlockSample27;
+        public short lastBlockSample28;
+
+        public short latest;
+
+        public bool hasSamples;
 
         public Voice() {
             adsrPhase = Phase.Off;
@@ -27,11 +78,12 @@ namespace ProjectPSX.Devices.Spu {
 
         public void keyOn() {
             currentAddress = startAddress;
+            adsrVolume = 0;
             adsrPhase = Phase.Attack;
         }
 
         public void keyOff() {
-            adsrPhase = Phase.Release;
+            adsrPhase = Phase.Off; //Todo this should be release but as no ADSR yet the voices never end
         }
 
         public enum Phase {
@@ -40,6 +92,136 @@ namespace ProjectPSX.Devices.Spu {
             Decay,
             Sustain,
             Release
+        }
+
+        public byte[] spuAdpcm = new byte[16];
+        public short[] decodedSamples = new short[28];
+        internal void decodeSamples(byte[] ram) {
+            //save the last 3 samples from the last decoded block
+            //this are needed for interpolation in case the index is 0 1 or 2
+            lastBlockSample28 = decodedSamples[decodedSamples.Length - 1];
+            lastBlockSample27 = decodedSamples[decodedSamples.Length - 2];
+            lastBlockSample26 = decodedSamples[decodedSamples.Length - 3];
+
+            Array.Copy(ram, currentAddress * 8, spuAdpcm, 0, 16);
+
+            int shift = 12 - (spuAdpcm[0] & 0x0F);
+            int filter = (spuAdpcm[0] & 0x70) >> 4; //filter on SPU adpcm is 0-4 vs XA wich is 0-3
+            if (filter > 4) filter = 4; //Crash Bandicoot sets this to 7 at the end of the first level and overflows the filter
+
+            int f0 = positiveXaAdpcmTable[filter];
+            int f1 = negativeXaAdpcmTable[filter];
+
+            //Actual ADPCM decoding is the same as on XA but the layout here is sequencial by nibble where on XA in grouped by nibble line
+            int position = 2; //skip shift and flags
+            int nibble = 1;
+            for (int i = 0; i < 28; i++) {
+                nibble = (nibble + 1) & 0x1;
+
+                int t = signed4bit((byte)((spuAdpcm[position] >> (nibble * 4)) & 0x0F));
+                int s = (t << shift) + ((old * f0 + older * f1 + 32) / 64);
+                short sample = (short)Math.Clamp(s, -0x8000, 0x7FFF);
+
+                decodedSamples[i] = sample;
+
+                older = old;
+                old = sample;
+
+                position += nibble;
+            }
+        }
+
+        public static int signed4bit(byte value) {
+            return (value << 28) >> 28;
+        }
+
+        internal short processVolume(Volume volume) {
+            if (!volume.isSweepMode) {
+                return volume.fixedVolume;
+            } else {
+                return 0; //todo handle sweep mode volume envelope
+            }
+        }
+
+        public short getSample(int i) {
+            if(i == -3) {
+                return lastBlockSample26;
+            } else if(i == -2) {
+                return lastBlockSample27;
+            } else if(i == -1) {
+                return lastBlockSample28;
+            } else {
+                return decodedSamples[i];
+            }
+        }
+
+        int adsrCounter;
+        internal void tickAdsr(int v) {
+            if (adsrPhase == Phase.Off) return;
+
+            int adsrTarget;
+            int adsrShift;
+            int adsrStep;
+            bool isDecreasing;
+            bool isExponential;
+
+            //Todo move out of tick the actual change of phase
+            switch (adsrPhase) {
+                case Phase.Attack:
+                    adsrTarget = 0x7FFF;
+                    adsrShift = adsr.attackShift;
+                    adsrStep = 7 - adsr.attackStep; // reg is 0-3 but values are "+7,+6,+5,+4"
+                    isDecreasing = false; // Allways increase till 0x7FFF
+                    isExponential = adsr.isAttackModeExponential;
+                    break;
+                case Phase.Decay:
+                    adsrTarget = (adsr.sustainLevel + 1) * 0x800;
+                    adsrShift = adsr.decayShift;
+                    adsrStep = -8;
+                    isDecreasing = true; // Allways decreases (till target)
+                    isExponential = true; // Allways exponential
+                    break;
+                case Phase.Sustain:
+                    adsrTarget = adsr.isSustainDirectionDecrease ? 0 : 0x7FFFF;
+                    adsrShift = adsr.sustainShift;
+                    adsrStep = adsr.isSustainDirectionDecrease? -8 + adsr.sustainStep: 7 - adsr.sustainStep;
+                    isDecreasing = adsr.isSustainDirectionDecrease; //till keyoff
+                    isExponential = adsr.isSustainModeExponential;
+                    break;
+                case Phase.Release:
+                    adsrTarget = 0;
+                    adsrShift = adsr.releaseShift;
+                    adsrStep = -8;
+                    isDecreasing = true; // Allways decrease till 0
+                    isExponential = adsr.isReleaseModeExponential;
+                    break;
+                default:
+                    adsrTarget = 0;
+                    adsrShift = 0;
+                    adsrStep = 0;
+                    isDecreasing = false;
+                    isExponential = false;
+                    break;
+            }
+
+            //Envelope Operation depending on Shift/Step/Mode/Direction
+            //AdsrCycles = 1 SHL Max(0, ShiftValue-11)
+            //AdsrStep = StepValue SHL Max(0,11-ShiftValue)
+            //IF exponential AND increase AND AdsrLevel>6000h THEN AdsrCycles=AdsrCycles*4    
+            //IF exponential AND decrease THEN AdsrStep = AdsrStep * AdsrLevel / 8000h
+            //Wait(AdsrCycles); cycles counted at 44.1kHz clock
+            //AdsrLevel=AdsrLevel+AdsrStep  ;saturated to 0..+7FFFh
+            if (adsrCounter > 0) { adsrCounter--; return; }
+
+            int envelopeCycles = 1 << Math.Max(0, adsrShift - 11);
+            int envelopeStep = adsrStep << Math.Max(0, 11 - adsrShift);
+            if(isExponential && !isDecreasing && adsrVolume > 0x6000) { envelopeCycles *= 4; }
+            if(isExponential && isDecreasing) { envelopeStep = envelopeStep * adsrVolume / 0x8000; }
+            adsrVolume = (short)Math.Clamp(adsrVolume + envelopeStep, 0, 0x7FFF);
+            adsrCounter = envelopeCycles;
+
+            bool nextPhase = isDecreasing ? (adsrVolume <= adsrTarget) : (adsrVolume >= adsrTarget);
+            if (nextPhase && adsrPhase != Phase.Sustain) adsrPhase++;
         }
     }
 }
