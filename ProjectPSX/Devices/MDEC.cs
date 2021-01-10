a2d031f0931eabae6f36c8561dd9798e65bef05b
Blue <bluestorm.android@gmail.com>
2021-01-10 13:07:31 +0100

    MDEC: Clean up IDCT, use ReadOnlySpan on ZagZig

--- ProjectPSX/Devices/MDEC.cs
+++ ProjectPSX/Devices/MDEC.cs
@@ -28,6 +28,8 @@ namespace ProjectPSX.Devices {
         private short[] Cbblk = new short[64];
         private short[][] Yblk = { new short[64], new short[64], new short[64], new short[64] };
 
+        short[] dst = new short[64];
+
         private ushort[] src = new ushort[0xFFFF]; //TEST TODO: REVISIT THIS "it works"...
         private uint[] inBuffer = new uint[0xFFFF]; //this is badly wrong. should be 0x20 and handle dmas
         private Queue<uint> outBuffer = new Queue<uint>();
@@ -169,13 +171,12 @@ namespace ProjectPSX.Devices {
 
                 i = i + ((n >> 10) & 0x3F) + 1;
 
-                val = (  ( signed10bit( (n & 0x3FF) ) * qt[i & 0x3F] * q_scale + 4) / 8    );
+                val = (signed10bit(n & 0x3FF) * qt[i & 0x3F] * q_scale + 4) / 8;
             }
-            idct_core(ref blk);
+            idct_core(blk);
         }
 
-        private void idct_core(ref short[] src) {
-            short[] dst = new short[64];
+        private void idct_core(short[] src) {
             for (int i = 0; i < 2; i++) {
                 for (int x = 0; x < 8; x++) {
                     for (int y = 0; y < 8; y++) {
@@ -186,9 +187,7 @@ namespace ProjectPSX.Devices {
                         dst[x + y * 8] = (short)((sum + 0xFFF) / 0x2000);
                     }
                 }
-                short[] tmp = dst;
-                dst = src;
-                src = tmp;
+                (dst, src) = (src, dst);
             }
         }
 
@@ -315,25 +314,16 @@ namespace ProjectPSX.Devices {
             return outBuffer.Count == 0;
         }
 
-        private static readonly byte[] zigzag = {
-              0 ,1 ,5 ,6 ,14,15,27,28,
-              2 ,4 ,7 ,13,16,26,29,42,
-              3 ,8 ,12,17,25,30,41,43,
-              9 ,11,18,24,31,40,44,53,
-              10,19,23,32,39,45,52,54,
-              20,22,33,38,46,51,55,60,
-              21,34,37,47,50,56,59,61,
-              35,36,48,49,57,58,62,63
+        private static ReadOnlySpan<byte> zagzig => new byte[] {
+             0,  1,  8, 16,  9,  2,  3, 10,
+            17, 24, 32, 25, 18, 11,  4,  5,
+            12, 19, 26, 33, 40, 48, 41, 34,
+            27, 20, 13,  6,  7, 14, 21, 28,
+            35, 42, 49, 56, 57, 50, 43, 36,
+            29, 22, 15, 23, 30, 37, 44, 51,
+            58, 59, 52, 45, 38, 31, 39, 46,
+            53, 60, 61, 54, 47, 55, 62, 63
         };
 
-        private static readonly byte[] zagzig = generateZagZig();
-
-        private static byte[] generateZagZig() {
-            byte[] zagzig = new byte[64];
-            for (byte i = 0; i < zagzig.Length; i++) {
-                zagzig[zigzag[i]] = i;
-            }
-            return zagzig;
-        }
     }
 }
