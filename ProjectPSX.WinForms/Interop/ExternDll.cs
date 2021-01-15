eac81c8c5936134f9a9ce06974fe7f443d282850
Blue <bluestorm.android@gmail.com>
2021-01-15 22:53:20 +0100

    Push Interop

---
+++ ProjectPSX.WinForms/Interop/ExternDll.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjectPSX
+{
+    internal static class ExternDll
+    {
+        public const string Gdi32 = "gdi32.dll";
+        public const string User32 = "user32.dll";
+    }
+}
---
+++ ProjectPSX.WinForms/Interop/GdiDeviceContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+using User32 = ProjectPSX.Interop.User32.NativeMethods;
+
+namespace ProjectPSX
+{
+    internal readonly ref struct GdiDeviceContext
+    {
+        private readonly IntPtr _handle;
+        private readonly IntPtr _hdc;
+
+        public GdiDeviceContext(IntPtr handle)
+        {
+            _handle = handle;
+            _hdc = User32.GetDC(handle);
+        }
+
+        public void Dispose()
+        {
+            User32.ReleaseDC(_handle, _hdc);
+        }
+
+        public static implicit operator IntPtr(GdiDeviceContext dc)
+        {
+            return dc._hdc;
+        }
+    }
+}
