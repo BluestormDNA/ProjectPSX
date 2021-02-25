0efcca7fd345743b7e6cad741c8e4ed8290e0b2d
Blue <bluestorm.android@gmail.com>
2021-02-25 20:20:42 +0100

    GTE: Handle RTPS/RTPT UNR Div Among other things this fixes: Metal Gear Solid water, Crash Team Racing broken polys, Ridge Racer 4 weird road below the car...

--- ProjectPSX/Core/GTE.cs
+++ ProjectPSX/Core/GTE.cs
@@ -39,7 +39,6 @@ namespace ProjectPSX {
             [FieldOffset(4)] public short z;
         }
 
-
         [StructLayout(LayoutKind.Explicit)]
         private struct Vector2 {
             [FieldOffset(0)] public uint val;
@@ -82,11 +81,11 @@ namespace ProjectPSX {
 
         //Command decode
         private int sf;                     //Shift fraction (0 or 12)
-        private uint MVMVA_M_Matrix;         //MVMVA Multiply Matrix    (0=Rotation. 1=Light, 2=Color, 3=Reserved)
-        private uint MVMVA_M_Vector;         //MVMVA Multiply Vector    (0=V0, 1=V1, 2=V2, 3=IR/long)
-        private uint MVMVA_T_Vector;         //MVMVA Translation Vector (0=TR, 1=BK, 2=FC/Bugged, 3=None)
-        private bool lm;                     //Saturate IR1,IR2,IR3 result (0=To -8000h..+7FFFh, 1=To 0..+7FFFh)
-        private uint opcode;                 //GTE opcode
+        private uint MVMVA_M_Matrix;        //MVMVA Multiply Matrix    (0=Rotation. 1=Light, 2=Color, 3=Reserved)
+        private uint MVMVA_M_Vector;        //MVMVA Multiply Vector    (0=V0, 1=V1, 2=V2, 3=IR/long)
+        private uint MVMVA_T_Vector;        //MVMVA Translation Vector (0=TR, 1=BK, 2=FC/Bugged, 3=None)
+        private bool lm;                    //Saturate IR1,IR2,IR3 result (0=To -8000h..+7FFFh, 1=To 0..+7FFFh)
+        private uint opcode;                //GTE opcode
         private CPU cpu;
         public GTE(CPU cpu) {//this is only needed for temporary debug purposes till TGTE passes
             this.cpu = cpu;
@@ -107,7 +106,7 @@ namespace ProjectPSX {
             FLAG = 0;
 
             switch (opcode) {
-                case 0x01: RTPS(0); break;
+                case 0x01: RTPS(0, true); break;
                 case 0x06: NCLIP(); break;
                 case 0x0C: OP(); break;
                 case 0x10: DPCS(false); break;
@@ -496,7 +495,6 @@ namespace ProjectPSX {
             MAC2 = (int)setMAC(2, (IR[2] * IR[2]) >> sf * 12);
             MAC3 = (int)setMAC(3, (IR[3] * IR[3]) >> sf * 12);
 
-
             IR[1] = setIR(1, MAC1, lm);
             IR[2] = setIR(2, MAC2, lm);
             IR[3] = setIR(3, MAC3, lm);
@@ -506,7 +504,7 @@ namespace ProjectPSX {
             //MAC0 = ZSF3 * (SZ1 + SZ2 + SZ3); for AVSZ3
             //OTZ = MAC0 / 1000h;for both(saturated to 0..FFFFh)
             long avsz3 = (long)ZSF3 * (SZ[1] + SZ[2] + SZ[3]);
-            MAC0 = setMAC0(avsz3);
+            MAC0 = (int)setMAC0(avsz3);
             OTZ = setSZ3(avsz3 >> 12);
         }
 
@@ -514,7 +512,7 @@ namespace ProjectPSX {
             //MAC0 = ZSF4 * (SZ0 + SZ1 + SZ2 + SZ3);for AVSZ4
             //OTZ = MAC0 / 1000h;for both(saturated to 0..FFFFh)
             long avsz4 = (long)ZSF4 * (SZ[0] + SZ[1] + SZ[2] + SZ[3]);
-            MAC0 = setMAC0(avsz4);
+            MAC0 = (int)setMAC0(avsz4);
             OTZ = setSZ3(avsz4 >> 12);
         }
 
@@ -587,51 +585,37 @@ namespace ProjectPSX {
         }
 
         private void NCLIP() { //Normal clipping
-            // MAC0 =   SX0*SY1 + SX1*SY2 + SX2*SY0 - SX0*SY2 - SX1*SY0 - SX2*SY1
-            MAC0 = setMAC0((long)SXY[0].x * SXY[1].y + SXY[1].x * SXY[2].y + SXY[2].x * SXY[0].y - SXY[0].x * SXY[2].y - SXY[1].x * SXY[0].y - SXY[2].x * SXY[1].y);
-        }
-        private int setMAC0(long value) {
-            if (value < -0x8000_0000) {
-                FLAG |= 0x8000;
-            } else if (value > 0x7FFF_FFFF) {
-                FLAG |= 0x1_0000;
-            }
-            return (int)value;
+            // MAC0 = SX0*SY1 + SX1*SY2 + SX2*SY0 - SX0*SY2 - SX1*SY0 - SX2*SY1
+            MAC0 = (int)setMAC0((long)SXY[0].x * SXY[1].y + SXY[1].x * SXY[2].y + SXY[2].x * SXY[0].y - SXY[0].x * SXY[2].y - SXY[1].x * SXY[0].y - SXY[2].x * SXY[1].y);
         }
 
         private void RTPT() { //Perspective Transformation Triple
-            RTPS(0);
-            RTPS(1);
-            RTPS(2);
+            RTPS(0, false);
+            RTPS(1, false);
+            RTPS(2, true);
         }
 
-        private void RTPS(int r) {
+        private void RTPS(int r, bool setMac0) {
             //IR1 = MAC1 = (TRX*1000h + RT11*VX0 + RT12*VY0 + RT13*VZ0) SAR (sf*12)
             //IR2 = MAC2 = (TRY*1000h + RT21*VX0 + RT22*VY0 + RT23*VZ0) SAR (sf*12)
             //IR3 = MAC3 = (TRZ*1000h + RT31*VX0 + RT32*VY0 + RT33*VZ0) SAR (sf*12)
+            MAC1 = (int)(setMAC(1, setMAC(1, setMAC(1, (long)TRX * 0x1000 + RT.v1.x * V[r].x) + (long)RT.v1.y * V[r].y) + (long)RT.v1.z * V[r].z) >> sf * 12);
+            MAC2 = (int)(setMAC(2, setMAC(2, setMAC(2, (long)TRY * 0x1000 + RT.v2.x * V[r].x) + (long)RT.v2.y * V[r].y) + (long)RT.v2.z * V[r].z) >> sf * 12);
+            long mac3 = setMAC(3, setMAC(3, setMAC(3, (long)TRZ * 0x1000 + RT.v3.x * V[r].x) + (long)RT.v3.y * V[r].y) + (long)RT.v3.z * V[r].z);
+            MAC3 = (int)(mac3 >> (sf * 12));
 
-            MAC1 = (int)setMAC(1, (TRX * 0x1000 + RT.v1.x * V[r].x + RT.v1.y * V[r].y + RT.v1.z * V[r].z) >> (sf * 12));
-            MAC2 = (int)setMAC(2, (TRY * 0x1000 + RT.v2.x * V[r].x + RT.v2.y * V[r].y + RT.v2.z * V[r].z) >> (sf * 12));
-            MAC3 = (int)setMAC(3, (TRZ * 0x1000 + RT.v3.x * V[r].x + RT.v3.y * V[r].y + RT.v3.z * V[r].z) >> (sf * 12));
-
-            IR[1] = setIR(1, MAC1, false);
-            IR[2] = setIR(2, MAC2, false);
-            IR[3] = setIR(3, MAC3, false);
+            IR[1] = setIR(1, MAC1, lm);
+            IR[2] = setIR(2, MAC2, lm);
+            setIR(3, (int)(mac3 >> 12), false);
+            IR[3] = (short)Math.Clamp(MAC3, lm ? 0 : -0x8000, 0x7FFF);
 
             //SZ3 = MAC3 SAR ((1-sf)*12)                           ;ScreenZ FIFO 0..+FFFFh
-
             SZ[0] = SZ[1];
             SZ[1] = SZ[2];
             SZ[2] = SZ[3];
-            SZ[3] = setSZ3(MAC3 >> ((1 - sf) * 12));
+            SZ[3] = setSZ3(mac3 >> 12);
 
-            //MAC0=(((H*20000h/SZ3)+1)/2)*IR1+OFX, SX2=MAC0/10000h ;ScrX FIFO -400h..+3FFh
-            //MAC0=(((H*20000h/SZ3)+1)/2)*IR2+OFY, SY2=MAC0/10000h ;ScrY FIFO -400h..+3FFh
-            //MAC0=(((H*20000h/SZ3)+1)/2)*DQA+DQB, IR0=MAC0/1000h  ;Depth cueing 0..+1000h
-
-            SXY[0] = SXY[1];
-            SXY[1] = SXY[2];
-
+            //NON UNR Div Version
             //long result;
             //long div;
             //if (SZ[3] == 0) {
@@ -646,8 +630,9 @@ namespace ProjectPSX {
             //        result = div;
             //    }
             //}
-
-            long n = 0;
+            
+            //UNR Div
+            long n;
             if (H < SZ[3] * 2) {
                 int z = BitOperations.LeadingZeroCount(SZ[3]) - 16;
                 n = H << z;
@@ -657,22 +642,28 @@ namespace ProjectPSX {
                 d = ((0x0000080 + (d * u)) >> 8);
                 n = (int)Math.Min(0x1FFFF, ((n * d) + 0x8000) >> 16);
             } else {
+                FLAG |= 1 << 17;
                 n = 0x1FFFF;
             }
 
-            MAC0 = (int)(n * IR[1] + OFX);
-            SXY[2].x = setSXY(2, MAC0 / 0x10000);
-            MAC0 = (int)(n * IR[2] + OFY);
-            SXY[2].y = setSXY(2, MAC0 / 0x10000);
-            MAC0 = (int)(n * DQA + DQB);
-            IR[0] = setIR0(MAC0 / 0x1000);
+            //MAC0=(((H*20000h/SZ3)+1)/2)*IR1+OFX, SX2=MAC0/10000h ;ScrX FIFO -400h..+3FFh
+            //MAC0=(((H*20000h/SZ3)+1)/2)*IR2+OFY, SY2=MAC0/10000h ;ScrY FIFO -400h..+3FFh
+            //MAC0=(((H*20000h/SZ3)+1)/2)*DQA+DQB, IR0=MAC0/1000h  ;Depth cueing 0..+1000h
+            int x = (int)(setMAC0(n * IR[1] + OFX) >> 16);
+            int y = (int)(setMAC0(n * IR[2] + OFY) >> 16);
 
+            SXY[0] = SXY[1];
+            SXY[1] = SXY[2];
+            SXY[2].x = setSXY(1, x);
+            SXY[2].y = setSXY(2, y);
 
-            // Console.WriteLine("RTPS " + ++rtpsTest + " FLAG " + FLAG.ToString("x8") + " SXY2 " + SXY[2].get().ToString("x8") + " SZ3 " + SZ[3].ToString("x8"));
-            // Console.WriteLine(((uint)MAC0).ToString("x8") + " " + ((uint)MAC1).ToString("x8") + " " + ((uint)MAC2).ToString("x8") + " " + ((uint)MAC3).ToString("x8"));
-            // Console.WriteLine(((uint)IR[0]).ToString("x8") + " " + ((uint)IR[1]).ToString("x8") + " " + ((uint)IR[2]).ToString("x8") + " " + ((uint)IR[3]).ToString("x8"));
-            //Console.ReadLine();
+            if (setMac0) {
+                long mac0 = setMAC0(n * DQA + DQB);
+                MAC0 = (int)mac0;
+                IR[0] = setIR0(mac0 >> 12);
+            }
         }
+
         private short setIR0(long value) {
             if (value < 0) {
                 FLAG |= 0x1000;
@@ -734,13 +725,20 @@ namespace ProjectPSX {
             return (short)value;
         }
 
+        private long setMAC0(long value) {
+            if (value < -0x8000_0000) {
+                FLAG |= 0x8000;
+            } else if (value > 0x7FFF_FFFF) {
+                FLAG |= 0x1_0000;
+            }
+            return value;
+        }
+
         private long setMAC(int i, long value) {
-            //Console.WriteLine((i) + " " + value.ToString("x16"));
+
             if (value < -0x800_0000_0000) {
-                //Console.WriteLine("under");
                 FLAG |= (uint)(0x800_0000 >> (i - 1));
             } else if (value > 0x7FF_FFFF_FFFF) {
-                //Console.WriteLine("over");
                 FLAG |= (uint)(0x4000_0000 >> (i - 1));
             }
 
@@ -765,55 +763,44 @@ namespace ProjectPSX {
         }
 
         internal uint loadData(uint fs) {
-            uint value;
-            switch (fs) {
-                case 00: value = V[0].XY; break;
-                case 01: value = (uint)V[0].z; break;
-                case 02: value = V[1].XY; break;
-                case 03: value = (uint)V[1].z; break;
-                case 04: value = V[2].XY; break;
-                case 05: value = (uint)V[2].z; break;
-                case 06: value = RGBC.val; break;
-                case 07: value = OTZ; break;
-                case 08: value = (uint)IR[0]; break;
-                case 09: value = (uint)IR[1]; break;
-                case 10: value = (uint)IR[2]; break;
-                case 11: value = (uint)IR[3]; break;
-                case 12: value = SXY[0].val; break;
-                case 13: value = SXY[1].val; break;
-                case 14: //Mirror
-                case 15: value = SXY[2].val; break;
-                case 16: value = SZ[0]; break;
-                case 17: value = SZ[1]; break;
-                case 18: value = SZ[2]; break;
-                case 19: value = SZ[3]; break;
-                case 20: value = RGB[0].val; break;
-                case 21: value = RGB[1].val; break;
-                case 22: value = RGB[2].val; break;
-                case 23: value = RES1; break; //Prohibited Register
-                case 24: value = (uint)MAC0; break;
-                case 25: value = (uint)MAC1; break;
-                case 26: value = (uint)MAC2; break;
-                case 27: value = (uint)MAC3; break;
-                case 28:/* value = IRGB; break;*/
-                case 29:/* value = ORGB; break;*/
-                    IRGB = (ushort)(saturateRGB(IR[3] / 0x80) << 10 | saturateRGB(IR[2] / 0x80) << 5 | saturateRGB(IR[1] / 0x80));
-                    value = IRGB;
-                    break;
-                case 30: value = (uint)LZCS; break;
-                case 31: value = (uint)LZCR; break;
-                default: value = 0xFFFF_FFFF; break;
-            }
-            //Console.WriteLine("GTE Load Data R" + fs + ": " + value.ToString("x8"));
-            //Console.WriteLine(value.ToString("x8"));
+            var value = fs switch {
+                00 => V[0].XY,
+                01 => (uint)V[0].z,
+                02 => V[1].XY,
+                03 => (uint)V[1].z,
+                04 => V[2].XY,
+                05 => (uint)V[2].z,
+                06 => RGBC.val,
+                07 => OTZ,
+                08 => (uint)IR[0],
+                09 => (uint)IR[1],
+                10 => (uint)IR[2],
+                11 => (uint)IR[3],
+                12 => SXY[0].val,
+                13 => SXY[1].val,
+                14 or 15 => SXY[2].val,
+                16 => SZ[0],
+                17 => SZ[1],
+                18 => SZ[2],
+                19 => SZ[3],
+                20 => RGB[0].val,
+                21 => RGB[1].val,
+                22 => RGB[2].val,
+                23 => RES1,
+                24 => (uint)MAC0,
+                25 => (uint)MAC1,
+                26 => (uint)MAC2,
+                27 => (uint)MAC3,
+                28 or 29 => IRGB = (ushort)(saturateRGB(IR[3] / 0x80) << 10 | saturateRGB(IR[2] / 0x80) << 5 | saturateRGB(IR[1] / 0x80)),
+                30 => (uint)LZCS,
+                31 => (uint)LZCR,
+                _ => 0xFFFF_FFFF,
+            };
+
             return value;
         }
 
-        private int test;
-        internal void writeData(uint fs, uint v) {
-            //Console.WriteLine("GTE Write Data R" + fs + ": " + v.ToString("x8"));
-            //Console.WriteLine(v.ToString("x8"));
-            //Console.ReadLine();
+        public void writeData(uint fs, uint v) {
             switch (fs) {
                 case 00: V[0].XY = v; break;
                 case 01: V[0].z = (short)v; break;
@@ -855,54 +842,47 @@ namespace ProjectPSX {
             }
         }
 
-        internal uint loadControl(uint fs) {
-            uint value;
-            switch (fs) {
-                case 00: value = RT.v1.XY; break;
-                case 01: value = (ushort)RT.v1.z | (uint)(RT.v2.x << 16); break;
-                case 02: value = (ushort)RT.v2.y | (uint)(RT.v2.z << 16); break;
-                case 03: value = RT.v3.XY; break;
-                case 04: value = (uint)RT.v3.z; break;
-                case 05: value = (uint)TRX; break;
-                case 06: value = (uint)TRY; break;
-                case 07: value = (uint)TRZ; break;
-                case 08: value = LM.v1.XY; break;
-                case 09: value = (ushort)LM.v1.z | (uint)(LM.v2.x << 16); break;
-                case 10: value = (ushort)LM.v2.y | (uint)(LM.v2.z << 16); break;
-                case 11: value = LM.v3.XY; break;
-                case 12: value = (uint)LM.v3.z; break;
-                case 13: value = (uint)RBK; break;
-                case 14: value = (uint)GBK; break;
-                case 15: value = (uint)BBK; break;
-                case 16: value = LRGB.v1.XY; break;
-                case 17: value = (ushort)LRGB.v1.z | (uint)(LRGB.v2.x << 16); break;
-                case 18: value = (ushort)LRGB.v2.y | (uint)(LRGB.v2.z << 16); break;
-                case 19: value = LRGB.v3.XY; break;
-                case 20: value = (uint)LRGB.v3.z; break;
-                case 21: value = (uint)RFC; break;
-                case 22: value = (uint)GFC; break;
-                case 23: value = (uint)BFC; break;
-                case 24: value = (uint)OFX; break;
-                case 25: value = (uint)OFY; break;
-                case 26: value = (uint)(short)H; break; //sign extend
-                case 27: value = (uint)DQA; break;
-                case 28: value = (uint)DQB; break;
-                case 29: value = (uint)ZSF3; break; //sign extend
-                case 30: value = (uint)ZSF4; break; //sign extend
-                case 31: value = FLAG; break;
-                default: value = 0xFFFF_FFFF; break;
-            }
-            //Console.WriteLine("GTE Load Cont R" + fs + ": " + value.ToString("x8"));
-            //Console.WriteLine(value.ToString("x8"));
+        public uint loadControl(uint fs) {
+            var value = fs switch {
+                00 => RT.v1.XY,
+                01 => (ushort)RT.v1.z | (uint)(RT.v2.x << 16),
+                02 => (ushort)RT.v2.y | (uint)(RT.v2.z << 16),
+                03 => RT.v3.XY,
+                04 => (uint)RT.v3.z,
+                05 => (uint)TRX,
+                06 => (uint)TRY,
+                07 => (uint)TRZ,
+                08 => LM.v1.XY,
+                09 => (ushort)LM.v1.z | (uint)(LM.v2.x << 16),
+                10 => (ushort)LM.v2.y | (uint)(LM.v2.z << 16),
+                11 => LM.v3.XY,
+                12 => (uint)LM.v3.z,
+                13 => (uint)RBK,
+                14 => (uint)GBK,
+                15 => (uint)BBK,
+                16 => LRGB.v1.XY,
+                17 => (ushort)LRGB.v1.z | (uint)(LRGB.v2.x << 16),
+                18 => (ushort)LRGB.v2.y | (uint)(LRGB.v2.z << 16),
+                19 => LRGB.v3.XY,
+                20 => (uint)LRGB.v3.z,
+                21 => (uint)RFC,
+                22 => (uint)GFC,
+                23 => (uint)BFC,
+                24 => (uint)OFX,
+                25 => (uint)OFY,
+                26 => (uint)(short)H,
+                27 => (uint)DQA,
+                28 => (uint)DQB,
+                29 => (uint)ZSF3,
+                30 => (uint)ZSF4,
+                31 => FLAG,
+                _ => 0xFFFF_FFFF,
+            };
+
             return value;
         }
 
-        internal void writeControl(uint fs, uint v) {
-            //Console.ForegroundColor = ConsoleColor.Red;
-            //Console.WriteLine(" >>>>>>>>>>>>>>>>>>>>       [TEST]" + ++test + "           <<<<<<<<<<<<<<<<<<<<<<<");
-            //Console.ResetColor();
-            //Console.WriteLine("GTE Write Control R" + fs + ": " + v.ToString("x8"));
-            //Console.WriteLine(v.ToString("x8"));
+        public void writeControl(uint fs, uint v) {
             switch (fs) {
                 case 00: RT.v1.XY = v; break;
                 case 01: RT.v1.z = (short)v; RT.v2.x = (short)(v >> 16); break;
